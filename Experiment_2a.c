5a00fc10da60387794e43df147fd32e71622c3f4
Pushkar-8976 <akshaygurav936@gmail.com>
2025-12-09 08:04:25 +0530

    Create Experiment_2a.c

---
+++ Experiment_2a.c
@@ -0,0 +1,19 @@
+/*NAME: PUSHKAR TANAJI KAKADE
+CLASS: ECS BRANCH
+DIVISION: E
+ROLL NO: 51
+UIN NO: 251S060
+*/
+#include <stdio.h>
+int main () {
+int x;
+printf("Enter a number: ");
+scanf("%d",&x);
+
+if (x%2==0)
+    printf("%d is even",x);
+else
+    printf("%d is odd",x);
+
+return 0;
+}
