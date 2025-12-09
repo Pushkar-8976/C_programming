dfbf2d2bd487e41bee9dcda0a98df2b54c1f8878
Pushkar-8976 <akshaygurav936@gmail.com>
2025-12-09 08:21:42 +0530

    Create Experiment_10a.c

---
+++ Experiment_10a.c
@@ -0,0 +1,23 @@
+/*NAME: PUSHKAR TANAJI KAKADE
+CLASS: ECS BRANCH
+DIVISION: E
+ROLL NO: 51
+UIN NO: 251S060
+*/
+#include <stdio.h>
+
+int main() {
+double num1, num2, sum;
+double *p1, *p2;
+
+p1 = &num1;
+p2 = &num2;
+
+printf("Enter two numbers: ");
+scanf("%lf %lf", p1, p2);
+
+sum = *p1 + *p2;
+
+printf("The sum is: %f\n", sum);
+return 0;
+}
