b8fbc27b86bdce79f470f946aea691f7823b31b2
Pushkar-8976 <akshaygurav936@gmail.com>
2025-12-09 08:17:46 +0530

    Create Experiment_8a.c

---
+++ Experiment_8a.c
@@ -0,0 +1,26 @@
+/*NAME: PUSHKAR TANAJI KAKADE
+CLASS: ECS BRANCH
+DIVISION: E
+ROLL NO: 51
+UIN NO: 251S060
+*/
+#include <stdio.h>
+
+int main() {
+char str[100];
+int i, length = 0;
+
+printf("Enter a string: ");
+fgets(str, 100, stdin);
+
+for (i = 0; str[i] != '\0'; i++) {
+    if (str[i] == '\n') {
+        str[i] = '\0';
+        break;
+    }
+    length++;
+}
+
+printf("Length of the string = %d\n", length);
+return 0;
+}
